e9980c9704dd8ed6ba8e241ee70da13006c7f065
braidenbraaten <braidenbraaten@gmail.com>
2016-05-16 09:13:51 -0700

    Fixed the first Puzzle!

--- Assets/Scripts/Prefabs/Puzzle_1.cs
+++ Assets/Scripts/Prefabs/Puzzle_1.cs
@@ -3,136 +3,85 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Puzzle_1 : MonoBehaviour {
-    //if you init in the editor area (aka , here)  then it will make the editor show what you inited rather than let you do
-    //what you wanted and then just override it later
-    public ObjectSwitchTemplate[] arrayOfTemplates = new ObjectSwitchTemplate[4]; 
 
-    //does it need to be public ?
-    Transform[] arrayOf_TemplateSockets = new Transform[4];
+    //the sets that contain the two different dim objects
+    public ObjectSwitchTemplate[] Sets;
 
-    public Transform[] arrayOf_GO_Pos = new Transform[4];
+    //the sockets th
+    public  Socket[] Sockets;
+    //the current number of rotations that have occured 
+    public int rotationIndex;
 
-    //determines if the dim1 object or dim2 object is going to be showing
-    public bool dim1Showing;
-    // should rotate the objects to the next location going counter clockwise
-    public bool rotateThings;
-
-	// Use this for initialization
-	void Start () {
-        for (int i = 0; i < arrayOfTemplates.Length; i++)
-        {
-            //setting the sockets pos to the GO's pos
-            arrayOf_TemplateSockets[i] = arrayOf_GO_Pos[i];
-        }
-
-
-        for (int i = 0; i < arrayOfTemplates.Length; i++)
-        {
-            //init'ing the sets to the sockets
-            arrayOfTemplates[i].transform.position = arrayOf_TemplateSockets[i].transform.position;
-        }
-        rotateThings = false;
-        dim1Showing = false;
-
-
-       
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
-        if (Input.GetKeyUp(KeyCode.T))
-            rotateThings = true;
-
-        //make sure that all of the bools in the templates obey the "Master bool" / the dim1Showing bool
-        for (int i = 0; i < arrayOfTemplates.Length; i++)
-        {
-            arrayOfTemplates[i].Dim1InView = dim1Showing;
-        }
-
-        if (rotateThings == true)
-        {
-            Debug.Log("Im rotating things!");
-            RotateStuff();
-            rotateThings = false;
-        }
-
-        //try and rotate the sets to the next position
-
-        for (int i = 0; i < arrayOf_TemplateSockets.Length; i++)
-        {
-            //init'ing the sets to the sockets
-            arrayOfTemplates[i].transform.position = arrayOf_TemplateSockets[i].transform.position;
-        }
-
-    }
-
-    //should only rotate things once
-    void RotateStuff()
+    void Start()
     {
-        for (int i = 0; i < arrayOf_TemplateSockets.Length; i++)
+        //sets to 0 so we can have a clean rotation
+        rotationIndex = 0;
+
+
+    }
+
+
+
+    void Update()
+    {
+        //the R key will rotate the objects Counter Clockwise 
+        if (Input.GetKeyUp(KeyCode.R) && rotationIndex < 4)
         {
-            if (arrayOf_TemplateSockets[i].position == arrayOf_GO_Pos[3].position) // pos 3
+            rotationIndex += 1;
+        } else if (rotationIndex >= 4)
+        {
+            rotationIndex = 0;
+        }
+
+        //the T key will make the objects switch between dimensions (1 and 2)
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            for (int i = 0; i < Sets.Length; i++)
             {
-                arrayOf_TemplateSockets[i].position = arrayOf_GO_Pos[0].position;
-            }
-            else if (arrayOf_TemplateSockets[i].position == arrayOf_GO_Pos[2].position) // pos 2
-            {
-                arrayOf_TemplateSockets[i].position = arrayOf_GO_Pos[3].position;
-            }
-            else if (arrayOf_TemplateSockets[i].position == arrayOf_GO_Pos[1].position) // pos 1
-            {
-                arrayOf_TemplateSockets[i].position = arrayOf_GO_Pos[2].position;
-            }
-            else if(arrayOf_TemplateSockets[i].position == arrayOf_GO_Pos[0].position) // pos 0
-            {
-                arrayOf_TemplateSockets[i].position = arrayOf_GO_Pos[1].position;
+                Sets[i].Dim1InView = !Sets[i].Dim1InView; 
             }
         }
 
+        RotateSets(rotationIndex);
 
-            
-        
-
-   
-
-     
-
-        //for (int i = 0; i < arrayOfTemplates.Length; i++)
-        //{
-        //    if (i + 1 == 4)
-        //    {
-        //        arrayOfTemplates[i].transform.position = arrayOf_TemplateSockets[0].position;
-            
-        //    }
-        //    else if(i + 1 < arrayOfTemplates.Length)
-        //    {
-        //        Debug.Log("I am going to the next object! bc i am in the last index of the socket");
-        //        arrayOfTemplates[i].transform.position = arrayOf_TemplateSockets[i + 1].transform.position;
-        //    }
-        //}
-
-        //for (int i = 0; i < arrayOfTemplates.Length; i++)
-        //{
-        //    if (arrayOfTemplates[i].transform.position == array_GO_Transforms[3].position)
-        //    {
-        //        arrayOfTemplates[i].transform.position = array_GO_Transforms[0].position;
-        //    }
-        //    else 
-        //    {
-        //        arrayOfTemplates[i].transform.position = array_GO_Transforms[i + 1].position;
-        //    }
-        //}
-
-
-        //for (int i = 0; i < arrayOfTemplates.Length; i++)
-        //{
-        //    if (array_GO_Transforms[i] == array_GO_Transforms[3])
-        //    {
-        //        array_GO_Transforms.SetValue(array_GO_Transforms[3], 0);
-        //    }else
-        //    arrayOfTemplates[i].transform.position = array_GO_Transforms[i + 1].transform.position;
-        //}
-        
     }
+
+    //this is a quick patch to our problem, if we have time to polish I sugest that we look for a more optimized solution
+    void RotateSets(int rotationNum)
+    {
+        switch (rotationNum)
+        {
+            case 0:
+                Sets[0].transform.position = Sockets[0].transform.position;
+                Sets[1].transform.position = Sockets[1].transform.position;
+                Sets[2].transform.position = Sockets[2].transform.position;
+                Sets[3].transform.position = Sockets[3].transform.position;
+                break;
+
+            case 1:
+                Sets[0].transform.position = Sockets[1].transform.position;
+                Sets[1].transform.position = Sockets[2].transform.position;
+                Sets[2].transform.position = Sockets[3].transform.position;
+                Sets[3].transform.position = Sockets[0].transform.position;
+
+                break;
+
+            case 2:
+                Sets[0].transform.position = Sockets[2].transform.position;
+                Sets[1].transform.position = Sockets[3].transform.position;
+                Sets[2].transform.position = Sockets[0].transform.position;
+                Sets[3].transform.position = Sockets[1].transform.position;
+
+                break;
+
+            case 3:
+                Sets[0].transform.position = Sockets[3].transform.position;
+                Sets[1].transform.position = Sockets[0].transform.position;
+                Sets[2].transform.position = Sockets[1].transform.position;
+                Sets[3].transform.position = Sockets[2].transform.position;
+
+                break;
+        }
+    }
+
 }
---
+++ Assets/Scripts/Prefabs/Socket.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Socket : MonoBehaviour {
+    
+    
+    
+    public Transform SocketPosition;
+  
+
+	// Use this for initialization
+	void Start () {
+       
+        this.transform.position = SocketPosition.position;
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+        //thisSet.transform.position = SocketPosition.position;
+
+	}
+
+    
+}
