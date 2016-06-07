64901798ac6997ea4033811cd45e3f0ab38e005f
Braiden Braaten <braiden.braaten@sea.aie.edu>
2016-06-06 18:01:13 -0700

    working on the second puzzle

--- Assets/Scripts/Prefabs/Puzzle 2Stuff/ItemLanes.cs
+++ Assets/Scripts/Prefabs/Puzzle 2Stuff/ItemLanes.cs
@@ -1,52 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// The lane class should:
+/// []Rotation Wheel
+/// []bool to check if when the keys are dropped, they are being dropped in the correct lane
+/// []
+/// </summary>
 
+    //the Lane Class
 public class ItemLanes : MonoBehaviour {
-    public GameObject Keys;
-    private float x; //used for the x value between -1 and 1 to determine which lane thee keys will fall into
     public enum LANE {VOID_LEFT, ONE,TWO,THREE,FOUR, VOID_RIGHT};
-    public LANE lane;
-	// Use this for initialization
-	void Start () {
-        
+    //the rotating wheel that holds the keys
+    public Rotation_Wheel wheel;
+    //use the keys to track where they are falling 
+    public GameObject keys;
+    //check to see if when the keys drop, if they are in the correct lane
+    bool inCorrectLane = false;
 
-    }
-	
-	
-	// Update is called once per frame
-	void Update () {
-        x = Keys.transform.position.x;
-	
-	}
-
-    void CheckLanes(float currPos)
+    void Start()
     {
-        if (currPos < (-2f / 3f)) //void Left
-        {
-            lane = LANE.VOID_LEFT;
-        }
-        else if ((-2f / 3f) <= x && x < (-1f / 3f)) //Lane 1
-        {
-            lane = LANE.ONE;
-        }
-        else if ((-1f / 3f) <= x && x < 0) // Lane 2
-        {
-            lane = LANE.TWO;
-        }
-        else if (0 <= x && x < (1f / 3f)) // Lane 3
-        {
-            lane = LANE.THREE;
-        }
-        else if ((1f / 3f) <= x && x < (2f / 3f))// Lane 4
-        {
-            lane = LANE.FOUR;
-        }
-        else if (x >= (2f / 3f))
-        {
-            lane = LANE.VOID_RIGHT; // Void Right
-        }
-
-
+        
     }
+
+    void Update() 
+    {
+        
+    }
+    
+
 }
--- Assets/Scripts/Prefabs/Puzzle 2Stuff/Rotation_Wheel.cs
+++ Assets/Scripts/Prefabs/Puzzle 2Stuff/Rotation_Wheel.cs
@@ -4,28 +4,150 @@ using System.Collections;
 /// This is the rotation wheel that will rotate the key object in either a circle motion or oval esc... 
 /// </summary>
 public class Rotation_Wheel : MonoBehaviour {
-    public bool hasStarted;
+    public bool hasStarted = false;
+    public bool dropped = false;
+    public float dropTimer = 0.0f;
+    private float prevDropTimer;
     public GameObject Wheel;
+    public Door puzzleDoor;
+    //the keys
+    public GameObject keys;
     //the position of the keys
-    public Transform Keypos;
+    //public Transform Keypos;
     //the position on the wheel that we want to hold the keys
     public Transform targetKeyPos;
-    public float rotSpeed = 2f;
+    //used for when we drop the keys
+    private Transform prevKeyPos;
+    public float rotSpeed = 20f;
+    public float jamSpeed = 30f;
+
+
     void Start()
     {
         hasStarted = false;
+        prevDropTimer = dropTimer;
     }
 
-    void Update()
+    public void Update()
     {
+        
+        prevKeyPos = targetKeyPos;
 
+   
 
+        //checks to see if we have gone through firstDrop yet
+        if (hasStarted)
+        {
+            //rotates the wheel
+            Rotate();
+
+           
+            //rotates the key object with the wheel
+            //RotateKey();
+        }
+        else if(hasStarted == false){
+            FirstDrop();
+        }
     }
 
     //the function that will enact the first drop / first player interaction
-    void FirstDrop()
+    public void FirstDrop()
     {
+        //makes the object slerp between two angle to look jammed
+        if (hasStarted == false)
+        {
+            if (puzzleDoor.open == false) { JammedRotate(); }
+        }
+        //this will cause the if statement in update to go off
+        if (puzzleDoor.open == true) { hasStarted = true;}
+    }
 
+    //function that allows the key to rotate with the wheel
+    public void RotateKey()
+    {
+        keys.transform.position = targetKeyPos.position;
+        keys.transform.forward = targetKeyPos.forward;
+        //makes sure that the key is the child of the wheel
+        targetKeyPos.SetParent(Wheel.transform);
+        //targetKeyPos.RotateAround(Wheel.transform.position, Vector3.up, -rotSpeed * Time.deltaTime);
+    }
+
+    public void DropKey()
+    {
+        dropped = true;
+        keys.transform.parent.SetParent(null);
+        if (dropped) { dropTimer -= Time.deltaTime;}
+        if (dropTimer <= 0)
+        {
+            dropTimer = prevDropTimer;
+            dropped = false;
+            
+
+        }
+
+
+        if (dropped == false)
+        {
+            ResetKey();
+        }
+    }
+
+    public void ResetKey()
+    {
+        RotateKey();
+        //keys.transform.position = targetKeyPos.position;
+        //keys.transform.rotation = targetKeyPos.rotation;
+    }
+
+    private bool rot_start = false;
+    //should cause the wheel to spin counter clockwise
+    public void Rotate()
+    { 
+        Wheel.transform.Rotate(0,-rotSpeed * Time.deltaTime, 0);
+        if (rot_start == false)
+        {
+            if (hasStarted)
+            {
+                ResetKey();
+                rot_start = true;
+            }
+        }
+
+    }
+
+
+    //the angles that the wheel lerps between
+    public float fwdAngle;
+    public float bwdAngle;
+    //to tell if it needs to go back or forth
+    private bool forth = true;
+    //the rate at wich the wheel will go back and forth
+    public float interpolSpeed;
+    //keeps track of time.deltatime
+    private float watchTimer;
+    //the jamm function
+    private void JammedRotate()
+    {
+        Quaternion forwardHit = Quaternion.Euler(0, fwdAngle, 0);
+        Quaternion backHit = Quaternion.Euler(0, bwdAngle, 0);
+        watchTimer += Time.deltaTime;
+        if (watchTimer >= interpolSpeed)
+        {
+            forth = !forth;
+            watchTimer = 0;
+        }
+
+        if (forth)
+        {
+            Wheel.transform.localRotation = Quaternion.Slerp(Wheel.transform.localRotation, forwardHit, jamSpeed * Time.deltaTime);
+        }
+
+        if (forth == false)
+        {
+            Wheel.transform.localRotation = Quaternion.Slerp(Wheel.transform.localRotation, backHit, jamSpeed * Time.deltaTime);
+        }
+
+        RotateKey();
     }
     //if the player interacts for the first time the keys drop and the lights go out
     
@@ -34,5 +156,9 @@ public class Rotation_Wheel : MonoBehaviour {
     //and have the keys position reset before they come back on (so it looks like they 
     //just reappeared there).
 
-
+    //-f jamm[*]
+    //-f dropKey[]
+    //-f startRotation[]
+    //-f put key back on wheel[]
+    //-f enable lanes[]
 }
